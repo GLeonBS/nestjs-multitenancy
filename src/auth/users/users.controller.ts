2cf2c94ad8a11a13935e5b0986bd6e08889c1084
Gabriel Leon <gabriel.leon@MacBook-Pro-de-irrahgroup09.local>
2025-10-26 14:55:28 -0300

    feat: 🎸 Add project

---
+++ src/auth/users/users.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+  @Post()
+  create(user) {
+    return this.userService.create(user);
+  }
+}
